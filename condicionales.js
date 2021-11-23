b6720472c7652458b511ec60f8f849de1a6b7506
AnaSilvap <92560459+AnaSilvap@users.noreply.github.com>
2021-11-23 14:35:31 -0500

    Add files via upload

---
+++ condicionales.js
@@ -0,0 +1,26 @@
+let masa=Number(prompt("Su peso en kg"))
+let estatura=Number(prompt("Su estatura en m"))
+let imc=masa/(estatura*estatura)
+let clasificacion
+if (imc <=18.5) {
+    clasificacion = "Insuficiencia ponderal"
+}else{
+    if (imc <= 24.9) {
+        clasificacion = "Intervalo normal"
+    }else{
+        if (imc <= 30) {
+            clasificacion = "Sobrepeso o preobesidad"
+        }else{
+            if (imc <= 34.9) {
+                clasificacion = "Obesidad de clase I"
+            }else{
+                if (imc <= 39.9) {
+                    clasificacion = "Obesidad de clase II"
+                }else{
+                    clasificacion = "Obesidad de clase III"
+                }
+            }
+        }
+    }
+}
+document.getElementById ("Respuesta").innerHTML= "Su IMC es "+imc+" y esta en la clasificación "+clasificacion
